81b566800aa22e4e1b8530b4ad6d01b45b981029
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-07-14 21:20:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# GPU temperature protection
+### Pause image generation when GPU temperature exceeds threshold
+this extension uses nvidia-smi to monitor GPU temperature at the end of each step, if temperature exceeds threshold pause image generation until criteria are met.
+
+## Requirements
+Only tested on systems with one Nvidia GPU
+
+## Installation
+- method 1: Install using extensions tab `Available` extensions
+- method 2: Install using extensions tab `Install from URL` by manually copying the repository URL
+```
+https://github.com/w-e-w/stable-diffusion-webui-GPU-temperature-protection.git
+```
+
+## Setting
+Settings can be found at `Setting` > `GPU Temperature`
+
+- `GPU temperature monitor minimum interval`
+    - checking temperature too often will reduce image generation performance
+    - set to `0` well effectively disable this extension
+    - to completely disable extension disable the file extension tab
+- `GPU sleep temperature`
+    - generation will pause if GPU core temperature exceeds this temperature
+- `GPU wake temperature`
+    - generation will continue to pause until temperature has drops below this temperature 
+    - setting a higher value than `GPU sleep temperature`will effectively disable this
+- `Sleep Time`
+    - seconds to sleep before checking temperature again
+- `Max sleep Time` 
+    - max number of seconds that it's allowed to pause
+    - generation will continue disregarding `GPU wake temperature` after the allotted time has passed
+    - set to `0` disable this limit allowing it to pause indefinitely
+- `Print GPU Core temperature while sleeping in terminal`
+    - print the GPU core temperature reading from nvidia-smi to console when generation is paused
+    - providing information
+
+## Notes
+- Temperature unit Celsius, Time unit seconds
+- To be honest I don't recommend anyone using this extension, if your GPU is having temperature issues and don't have the means to improve the cooling, then recommend using software like MSI afterburner to undervolt or further power limit or thermal limit the GPU.
---
+++ scripts/gpu_temperature_protection.py
@@ -0,0 +1,72 @@
+from modules.sd_samplers_kdiffusion import KDiffusionSampler
+from modules import scripts, shared
+import gradio as gr
+import subprocess
+import time
+
+shared.options_templates.update(shared.options_section(('GPU_temperature_protection', "GPU Temperature"), {
+    "gpu_temps_sleep_enable": shared.OptionInfo(True, "Enable GPU temperature protection"),
+    "gpu_temps_sleep_print": shared.OptionInfo(True, "Print GPU Core temperature while sleeping in terminal"),
+    "gpu_temps_sleep_minimum_interval": shared.OptionInfo(5.0, "GPU temperature monitor minimum interval", gr.Number).info("won't check the temperature again until this amount of seconds have passed"),
+    "gpu_temps_sleep_sleep_time": shared.OptionInfo(1.0, "Sleep Time", gr.Number).info("seconds to pause before checking temperature again"),
+    "gpu_temps_sleep_max_sleep_time": shared.OptionInfo(10.0, "Max sleep Time", gr.Number).info("max number of seconds that it's allowed to pause, 0=unlimited"),
+    "gpu_temps_sleep_sleep_temp": shared.OptionInfo(75.0, "GPU sleep temperature", gr.Slider, {"minimum": 0, "maximum": 125}).info("generation will pause if GPU core temperature exceeds this temperature"),
+    "gpu_temps_sleep_wake_temp": shared.OptionInfo(75.0, "GPU wake temperature", gr.Slider, {"minimum": 0, "maximum": 125}).info("generation will pause until GPU core temperature drops below this temperature"),
+}))
+
+
+class GPUTemperatureProtection(scripts.Script):
+    def title(self):
+        return "GPU temperature protection"
+
+    def show(self, is_img2img):
+        return scripts.AlwaysVisible
+
+    def process(self, p, *args):
+        if shared.opts.gpu_temps_sleep_enable:
+            setattr(KDiffusionSampler, "callback_state", GPUTemperatureProtection.gpu_temperature_protection_decorator(KDiffusionSampler.callback_state))
+
+    @staticmethod
+    def get_gpu_temperature():
+        try:
+            return int(subprocess.check_output(
+                ['nvidia-smi', '--query-gpu=temperature.gpu', '--format=csv,noheader']).decode().strip())
+        except subprocess.CalledProcessError as e:
+            print(f"[Error GPU temperature protection]: {e.output.decode('utf-8').strip()}")
+        except Exception as e:
+            print(f'[Error GPU temperature protection]: {e}')
+        return 0
+
+    @staticmethod
+    def gpu_temperature_protection():
+        if shared.opts.gpu_temps_sleep_enable:
+            call_time = time.time()
+            if call_time - GPUTemperatureProtection.last_call_time > shared.opts.gpu_temps_sleep_minimum_interval:
+                gpu_core_temp = GPUTemperatureProtection.get_gpu_temperature()
+                if gpu_core_temp > shared.opts.gpu_temps_sleep_sleep_temp:
+
+                    if shared.opts.gpu_temps_sleep_print:
+                        print(f'\n\nGPU Temperature: {gpu_core_temp}')
+
+                    time.sleep(shared.opts.gpu_temps_sleep_sleep_time)
+                    gpu_core_temp = GPUTemperatureProtection.get_gpu_temperature()
+                    while gpu_core_temp > shared.opts.gpu_temps_sleep_wake_temp and (not shared.opts.gpu_temps_sleep_max_sleep_time or shared.opts.gpu_temps_sleep_max_sleep_time > time.time() - call_time) and shared.opts.gpu_temps_sleep_enable:
+                        if shared.opts.gpu_temps_sleep_print:
+                            print(f'GPU Temperature: {gpu_core_temp}')
+
+                        time.sleep(shared.opts.gpu_temps_sleep_sleep_time)
+                        gpu_core_temp = GPUTemperatureProtection.get_gpu_temperature()
+
+                    GPUTemperatureProtection.last_call_time = time.time()
+                else:
+                    GPUTemperatureProtection.last_call_time = call_time
+
+    @staticmethod
+    def gpu_temperature_protection_decorator(fun):
+        def wrapper(*args, **kwargs):
+            result = fun(*args, **kwargs)
+            GPUTemperatureProtection.gpu_temperature_protection()
+            return result
+        return wrapper
+
+    last_call_time = time.time()
