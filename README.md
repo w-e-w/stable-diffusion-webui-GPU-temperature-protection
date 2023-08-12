857d774208951abd5a1bc288b50404e2609182aa
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-08-13 04:53:17 +0900

    multi GPU support

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 this extension uses nvidia-smi to monitor GPU temperature at the end of each step, if temperature exceeds threshold pause image generation until criteria are met.
 
 ## Requirements
-Only tested on systems with one Nvidia GPU
+systems using Nvidia GPU
 
 ## Installation
 - method 1: Install using extensions tab `Available` extensions
@@ -33,6 +33,9 @@ Settings can be found at `Setting` > `GPU Temperature`
 - `Print GPU Core temperature while sleeping in terminal`
     - print the GPU core temperature reading from nvidia-smi to console when generation is paused
     - providing information
+- `GPU device index`
+    - selecting the correct temperature reading for multi GPU systems
+    - in most cases and for single GPU system this value should be `0`
 
 ## Notes
 - Temperature unit Celsius, Time unit seconds
--- scripts/gpu_temperature_protection.py
+++ scripts/gpu_temperature_protection.py
@@ -12,6 +12,7 @@ shared.options_templates.update(shared.options_section(('GPU_temperature_protect
     "gpu_temps_sleep_max_sleep_time": shared.OptionInfo(10.0, "Max sleep Time", gr.Number).info("max number of seconds that it's allowed to pause, 0=unlimited"),
     "gpu_temps_sleep_sleep_temp": shared.OptionInfo(75.0, "GPU sleep temperature", gr.Slider, {"minimum": 0, "maximum": 125}).info("generation will pause if GPU core temperature exceeds this temperature"),
     "gpu_temps_sleep_wake_temp": shared.OptionInfo(75.0, "GPU wake temperature", gr.Slider, {"minimum": 0, "maximum": 125}).info("generation will pause until GPU core temperature drops below this temperature"),
+    "gpu_temps_sleep_gpu_index": shared.OptionInfo(0, "GPU device index", gr.Number, {"minimum": 0, "precision": 0}).info("selecting the correct temperature reading for multi GPU systems"),
 }))
 
 
@@ -30,7 +31,7 @@ class GPUTemperatureProtection(scripts.Script):
     def get_gpu_temperature():
         try:
             return int(subprocess.check_output(
-                ['nvidia-smi', '--query-gpu=temperature.gpu', '--format=csv,noheader']).decode().strip())
+                ['nvidia-smi', '--query-gpu=temperature.gpu', '--format=csv,noheader']).decode().strip().splitlines()[shared.opts.gpu_temps_sleep_gpu_index])
         except subprocess.CalledProcessError as e:
             print(f"[Error GPU temperature protection]: {e.output.decode('utf-8').strip()}")
         except Exception as e:
