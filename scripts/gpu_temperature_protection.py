f59cf06882d46f1708aa4da5ece9db3ed16b88ad
w-e-w <40751091+w-e-w@users.noreply.github.com>
2023-08-26 16:11:40 +0900

    selectable platform

--- scripts/gpu_temperature_protection.py
+++ scripts/gpu_temperature_protection.py
@@ -5,6 +5,7 @@ import subprocess
 import time
 
 shared.options_templates.update(shared.options_section(('GPU_temperature_protection', "GPU Temperature"), {
+    "gpu_temps_sleep_temperature_src": shared.OptionInfo("NVIDIA - nvidia-smi", "Temperature source mode", gr.Radio, {"choices": ["NVIDIA - nvidia-smi"]}),
     "gpu_temps_sleep_enable": shared.OptionInfo(True, "Enable GPU temperature protection"),
     "gpu_temps_sleep_print": shared.OptionInfo(True, "Print GPU Core temperature while sleeping in terminal"),
     "gpu_temps_sleep_minimum_interval": shared.OptionInfo(5.0, "GPU temperature monitor minimum interval", gr.Number).info("won't check the temperature again until this amount of seconds have passed"),
@@ -25,10 +26,13 @@ class GPUTemperatureProtection(scripts.Script):
 
     def process(self, p, *args):
         if shared.opts.gpu_temps_sleep_enable:
-            setattr(KDiffusionSampler, "callback_state", GPUTemperatureProtection.gpu_temperature_protection_decorator(KDiffusionSampler.callback_state))
+            setattr(KDiffusionSampler, "callback_state", GPUTemperatureProtection.gpu_temperature_protection_decorator(
+                KDiffusionSampler.callback_state,
+                GPUTemperatureProtection.get_temperature_src_function(shared.opts.gpu_temps_sleep_temperature_src)
+            ))
 
     @staticmethod
-    def get_gpu_temperature():
+    def get_gpu_temperature_nvidia_smi():
         try:
             return int(subprocess.check_output(
                 ['nvidia-smi', '--query-gpu=temperature.gpu', '--format=csv,noheader']).decode().strip().splitlines()[shared.opts.gpu_temps_sleep_gpu_index])
@@ -38,35 +42,43 @@ class GPUTemperatureProtection(scripts.Script):
             print(f'[Error GPU temperature protection]: {e}')
         return 0
 
+    temperature_src_dict = {
+        "NVIDIA - nvidia-smi": get_gpu_temperature_nvidia_smi
+    }
+
     @staticmethod
-    def gpu_temperature_protection():
+    def get_temperature_src_function(source_name):
+        return GPUTemperatureProtection.temperature_src_dict.get(source_name, GPUTemperatureProtection.get_gpu_temperature_nvidia_smi)
+
+    @staticmethod
+    def gpu_temperature_protection(temperature_src_fun):
         if shared.opts.gpu_temps_sleep_enable:
             call_time = time.time()
             if call_time - GPUTemperatureProtection.last_call_time > shared.opts.gpu_temps_sleep_minimum_interval:
-                gpu_core_temp = GPUTemperatureProtection.get_gpu_temperature()
+                gpu_core_temp = temperature_src_fun()
                 if gpu_core_temp > shared.opts.gpu_temps_sleep_sleep_temp:
 
                     if shared.opts.gpu_temps_sleep_print:
                         print(f'\n\nGPU Temperature: {gpu_core_temp}')
 
                     time.sleep(shared.opts.gpu_temps_sleep_sleep_time)
-                    gpu_core_temp = GPUTemperatureProtection.get_gpu_temperature()
+                    gpu_core_temp = temperature_src_fun()
                     while gpu_core_temp > shared.opts.gpu_temps_sleep_wake_temp and (not shared.opts.gpu_temps_sleep_max_sleep_time or shared.opts.gpu_temps_sleep_max_sleep_time > time.time() - call_time) and shared.opts.gpu_temps_sleep_enable:
                         if shared.opts.gpu_temps_sleep_print:
                             print(f'GPU Temperature: {gpu_core_temp}')
 
                         time.sleep(shared.opts.gpu_temps_sleep_sleep_time)
-                        gpu_core_temp = GPUTemperatureProtection.get_gpu_temperature()
+                        gpu_core_temp = temperature_src_fun()
 
                     GPUTemperatureProtection.last_call_time = time.time()
                 else:
                     GPUTemperatureProtection.last_call_time = call_time
 
     @staticmethod
-    def gpu_temperature_protection_decorator(fun):
+    def gpu_temperature_protection_decorator(fun, temperature_src_fun):
         def wrapper(*args, **kwargs):
             result = fun(*args, **kwargs)
-            GPUTemperatureProtection.gpu_temperature_protection()
+            GPUTemperatureProtection.gpu_temperature_protection(temperature_src_fun)
             return result
         return wrapper
 
