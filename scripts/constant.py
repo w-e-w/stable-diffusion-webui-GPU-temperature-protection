0797c48a43146918675b90e88ae942e5f1cb144f
ibrahem <qlp@live.in>
2023-09-03 20:58:44 +0300

    add download OpenHardwareMonitorLib script

--- scripts/constant.py
+++ scripts/constant.py
@@ -1,6 +1,9 @@
 import os
 from pathlib import Path
 
+#OpenHardwareMonitorLib download url
+OpenHardwareMonitorLibDownloadUrl = "https://openhardwaremonitor.org/files/openhardwaremonitor-v0.9.6.zip"
+
 extensionRootPath = os.path.join(Path().absolute(),'./extensions/stable-diffusion-webui-GPU-temperature-protection/')
 
 # folder path
@@ -8,3 +11,6 @@ openHardwareMonitorDirPath = os.path.join(extensionRootPath,'./openHardwareMonit
 
 #path to load openHardwareMonitorLib using pythonnet
 openHardwareMonitorLibPath = os.path.join(openHardwareMonitorDirPath,'./OpenHardwareMonitorLib',)
+
+#dll path need for openhardwaremonitor zip extact
+OpenHardwareMonitorLibdllFilePath = os.path.join(openHardwareMonitorDirPath,'./OpenHardwareMonitorLib.dll')
--- scripts/install.py
+++ scripts/install.py
@@ -1,7 +1,22 @@
 import launch
 import os
-
+from modules import shared
+from pathlib import Path
+from scripts import constant
+import urllib
+import zipfile
 
 # install pythonnet required for openHardwareMonitor Lib
-if not launch.is_installed("pythonnet") and os.name == 'nt':
-    launch.run_pip("install pythonnet==3.0.2", "requirements for windows OpenHardwareMonitorLib")
+if shared.opts.gpu_temps_sleep_temperature_src == 'NVIDIA & AMD - openHardwareMonitor' and os.name == 'nt':
+    if not launch.is_installed("pythonnet"):
+        launch.run_pip("install pythonnet==3.0.2", "requirements for windows OpenHardwareMonitorLib")
+    
+    # check and create OpenHardwareMonitor folder
+    Path(constant.openHardwareMonitorDirPath).mkdir(parents=True, exist_ok=True)
+
+    # check is OpenHardwareMonitorLib exist if not will download 
+    if not os.path.isfile(constant.OpenHardwareMonitorLibdllFilePath):
+        zip_path, _ = urllib.request.urlretrieve(constant.OpenHardwareMonitorLibDownloadUrl)
+        with zipfile.ZipFile(zip_path, "r") as z:
+            with open(os.path.realpath(constant.OpenHardwareMonitorLibdllFilePath) , 'wb') as f:
+                f.write(z.read('OpenHardwareMonitor/OpenHardwareMonitorLib.dll'))
