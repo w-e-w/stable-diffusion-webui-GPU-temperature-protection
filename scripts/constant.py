33c19aabcd30693fcf2b0df4282abe56e6173181
ibrahem <qlp@live.in>
2023-09-11 23:33:18 +0300

    add settings json path and resolve all paths

--- scripts/constant.py
+++ scripts/constant.py
@@ -4,13 +4,16 @@ from pathlib import Path
 #OpenHardwareMonitorLib download url
 OpenHardwareMonitorLibDownloadUrl = "https://openhardwaremonitor.org/files/openhardwaremonitor-v0.9.6.zip"
 
-extensionRootPath = os.path.join(Path().absolute(),'./extensions/stable-diffusion-webui-GPU-temperature-protection/')
+extensionRootPath = os.path.abspath(os.path.join(Path().absolute(),'./extensions/stable-diffusion-webui-GPU-temperature-protection/'))
+
+#path of settings Storage
+settingsStorageJsonPath = os.path.abspath(os.path.join(extensionRootPath,'./settingsStorage.json'))
 
 # folder path
-openHardwareMonitorDirPath = os.path.join(extensionRootPath,'./openHardwareMonitor')
+openHardwareMonitorDirPath = os.path.abspath(os.path.join(extensionRootPath,'./openHardwareMonitor'))
 
 #path to load openHardwareMonitorLib using pythonnet
-openHardwareMonitorLibPath = os.path.join(openHardwareMonitorDirPath,'./OpenHardwareMonitorLib',)
+openHardwareMonitorLibPath = os.path.abspath(os.path.join(openHardwareMonitorDirPath,'./OpenHardwareMonitorLib'))
 
 #dll path need for openhardwaremonitor zip extact
-OpenHardwareMonitorLibdllFilePath = os.path.join(openHardwareMonitorDirPath,'./OpenHardwareMonitorLib.dll')
+OpenHardwareMonitorLibdllFilePath = os.path.abspath(os.path.join(openHardwareMonitorDirPath,'./OpenHardwareMonitorLib.dll'))
