e214fc3ebe6f9ddd7b26c4237f01510c8a8753d0
w-e-w <40751091+w-e-w@users.noreply.github.com>
2025-05-19 14:03:06 +0900

    simplify

--- temperature_sensor_modules/nvidia_smi.py
+++ temperature_sensor_modules/nvidia_smi.py
@@ -4,8 +4,8 @@ import subprocess
 
 def get_gpu_temperature_nvidia_smi():
     try:
-        return int(subprocess.check_output(
-            ['nvidia-smi', '--query-gpu=temperature.gpu', '--format=csv,noheader']).decode().strip().splitlines()[shared.opts.gpu_temps_sleep_gpu_index])
+        return float(subprocess.check_output(
+            ['nvidia-smi', '--query-gpu=temperature.gpu', '--format=csv,noheader'], text=True).splitlines()[shared.opts.gpu_temps_sleep_gpu_index])
     except subprocess.CalledProcessError as e:
         print(f"\n[Error GPU temperature protection] nvidia-smi: {e.output.decode('utf-8').strip()}")
     except Exception as e:
